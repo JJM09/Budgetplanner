b20a2047e1881410ef36fa41a5540a8068be273c
JJM09 <73259422+JJM09@users.noreply.github.com>
2020-11-01 19:44:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VactionBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            username = "Jay";
+
+            string destination;
+            destination = "Canada";
+
+            string destination2;
+            destination2 = "Japan";
+
+            Console.WriteLine("Welcome to the Vaction Budget Planner");
+
+            Console.WriteLine("Please enter your username");
+
+            Console.WriteLine("Welcome " + username );
+
+            Console.WriteLine("are you traveling to " + destination + "or" + destination2 + "?");
+
+            Console.ReadLine();
+                
+
+        }
+    }
+}
